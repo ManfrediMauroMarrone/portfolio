ec53439708730902753a7d038aeb643f1a26c7aa
Manfredi <manfredi.marrone@hotmail.it>
2025-01-13 11:38:59 +0100

    aggiunto testo a comparsa

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,7 +1,35 @@
 import Container from "./Container";
-import React from "react";
+import React, { createRef, useEffect, useRef } from "react";
 
 function Intro() {
+  const titleRef = useRef(null);
+
+  useEffect(() => {
+    // Wrap every letter in a span
+
+    titleRef.current.innerHTML = titleRef.current.textContent.replace(
+      /\S/g,
+      "<span class='letter'>$&</span>"
+    );
+
+    anime
+      .timeline({ loop: true })
+      .add({
+        targets: ".letter",
+        opacity: [0, 1],
+        easing: "easeInOutQuad",
+        duration: 2250,
+        delay: (el, i) => 100 * (i + 1),
+      })
+      .add({
+        targets: ".ml3",
+        opacity: 0,
+        duration: 1000,
+        easing: "easeOutExpo",
+        delay: 1000,
+      });
+  }, []);
+
   return (
     <>
       <section
@@ -11,7 +39,7 @@ function Intro() {
         <Container>
           <div className="flex flex-col lg:flex-row h-56 lg:h-sectionHeight">
             <div className="lg:w-1/2 h-full flex items-center justify-center">
-              <h2 className="whitespace-pre-line">
+              <h2 className="ml3 whitespace-pre-line" ref={titleRef}>
                 Hi, I am Manfredi Marrone.{"\n"}A Full Stack Web Developer.
               </h2>
             </div>
@@ -32,7 +60,10 @@ function Intro() {
         viewBox="0 0 1440 79"
         xmlns="http://www.w3.org/2000/svg"
       >
-        <path d="M-100 79C-100 79 218.416 23.165 693.5 23.165C1168.58 23.165 1487 79 1487 79V0H-100V79Z" fill="#008080"></path>
+        <path
+          d="M-100 79C-100 79 218.416 23.165 693.5 23.165C1168.58 23.165 1487 79 1487 79V0H-100V79Z"
+          fill="#008080"
+        ></path>
       </svg>
     </>
   );
