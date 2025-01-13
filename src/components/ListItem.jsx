8d88575c4dca2d08e85e63c2b4e0f236c131662e
Manfredi <manfredi.marrone@hotmail.it>
2025-01-14 00:17:57 +0100

    changed stiles experienve section

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -3,8 +3,8 @@ import React from "react";
 function ListItem({company, title, date, content}) {
   return (
     <li>
-      <h3 className="text-2xl font-bold pt-8">{company}</h3>
-      <p>{title}</p>
+      <h3 className="text-2xl font-bold pt-8 text-primary">{company}</h3>
+      <p className="underline">{title}</p>
       <p>{date}</p>
       <ul>
         {content.map((e, index) => {
