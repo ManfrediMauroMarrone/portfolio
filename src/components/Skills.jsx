e644ce70d5ef3022a3b46f628ed25f85c179c5de
Manfredi <manfredi.marrone@hotmail.it>
2025-01-13 13:04:09 +0100

    imported skills images

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -2,6 +2,19 @@ import React from "react";
 import Title from "./Title";
 import { Swiper, SwiperSlide } from "swiper/react";
 
+//images
+
+import bootstrap from "../assets/images/bootstrap.png"
+import css from "../assets/images/css.png"
+import git from "../assets/images/git.png"
+import html from "../assets/images/html.png"
+import js from "../assets/images/js.png"
+import node from "../assets/images/node.png"
+import react from "../assets/images/react.png"
+import sass from "../assets/images/sass.png"
+import tailwind from "../assets/images/tailwind.png"
+
+
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/effect-coverflow";
@@ -43,32 +56,32 @@ function Skills() {
           className="mySwiper"
         >
           <SwiperSlide>
-            <img src="src\assets\images\html.png" />
+            <img src={html} />
           </SwiperSlide>
           <SwiperSlide>
-            <img src="src\assets\images\css.png" />
+          <img src={css} />
           </SwiperSlide>
           <SwiperSlide>
-            <img src="src\assets\images\js.png" />
+          <img src={js} />
           </SwiperSlide>
           <SwiperSlide>
-            <img src="src\assets\images\react.png" />
+          <img src={react} />
           </SwiperSlide>
           <SwiperSlide>
-            <img src="src\assets\images\node.png" />
+          <img src={node} />
           </SwiperSlide>
           <SwiperSlide>
-            <img src="src\assets\images\bootstrap.png" />
+          <img src={bootstrap} />
           </SwiperSlide>
           <SwiperSlide>
-            <img src="src\assets\images\tailwind.png" />
+          <img src={tailwind} />
           </SwiperSlide>
           <SwiperSlide>
-            <img src="src\assets\images\git.png" />
+          <img src={git} />
           </SwiperSlide>
           <SwiperSlide>
             <div className="h-24 lg:h-80 flex items-center">
-              <img src="src\assets\images\sass.png" />
+            <img src={sass} />
             </div>
           </SwiperSlide>
         </Swiper>
